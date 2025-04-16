4143531bfdc3d278fec74cc4dd869ef6033d298c
LinaKind <l.kinduryte@elifesciences.org>
2025-04-16 12:55:25 +0300

    Create the correct response headers in preparation for returning them
    https://github.com/elifesciences/issues/issues/9245

--- data_hub_metrics_api/api_router.py
+++ data_hub_metrics_api/api_router.py
@@ -1,5 +1,5 @@
 from typing import Sequence
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
 from data_hub_metrics_api.api_router_typing import CitationsResponseSequence
 from data_hub_metrics_api.citations_provider import CitationsProvider
@@ -10,6 +10,8 @@ def create_api_router(citations_provider_list: Sequence[CitationsProvider]) -> A
 
     @router.get('/metrics/article/{article_id}/citations/version/{version_number}')
     def provide_citations(article_id: str, version_number: int) -> CitationsResponseSequence:
+        response = Response()
+        response.headers['Content-Type'] = 'application/vnd.elife.metric-citations+json; version=2'
         return [
             citations_provider.get_citations_source_metric_for_article_id_and_version(
                 article_id=article_id,
