52a778593dfaa7bdf1f8b1349cd02582fcc78387
Daniel Ecer <de-code@users.noreply.github.com>
2025-04-22 17:17:41 +0100

    Added typing hint to get_citations_provider_list

--- data_hub_metrics_api/main.py
+++ data_hub_metrics_api/main.py
@@ -1,17 +1,18 @@
 import logging
+from typing import Sequence
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from data_hub_metrics_api.api_router import create_api_router
-from data_hub_metrics_api.citations_provider import DummyCitationsProvider
+from data_hub_metrics_api.citations_provider import CitationsProvider, DummyCitationsProvider
 from data_hub_metrics_api.crossref_citations_provider import CrossrefCitationsProvider
 
 
 LOGGER = logging.getLogger(__name__)
 
 
-def get_citations_provider_list():
+def get_citations_provider_list() -> Sequence[CitationsProvider]:
     return [
         CrossrefCitationsProvider(name="Crossref"),
         DummyCitationsProvider(name="PubMed Central"),
