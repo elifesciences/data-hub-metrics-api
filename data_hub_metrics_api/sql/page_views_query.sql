cf324be3eac1d4f32631114e5c6a3c742db26448
Daniel Ecer <de-code@users.noreply.github.com>
2025-06-18 13:29:53 +0100

    Temporarily limit data range due to it taking too long

--- data_hub_metrics_api/sql/page_views_query.sql
+++ data_hub_metrics_api/sql/page_views_query.sql
@@ -5,4 +5,5 @@ SELECT
 FROM `elife-data-pipeline.prod.ga4_metrics_event_counts_by_date` 
 WHERE article_id IS NOT NULL
   AND event_name = 'page_view'
+  AND event_date >= '2025-06-01'
 GROUP BY event_date, article_id
