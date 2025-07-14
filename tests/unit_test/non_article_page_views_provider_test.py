9a67e6ab05a395a6067834c6d3e6a6482351b552
Daniel Ecer <de-code@users.noreply.github.com>
2025-07-14 15:09:32 +0100

    Added tests for provider

---
+++ tests/unit_test/non_article_page_views_provider_test.py
@@ -0,0 +1,74 @@
+from unittest.mock import ANY, MagicMock
+
+import pytest
+
+from data_hub_metrics_api.api_router_typing import ContentTypeLiteral
+from data_hub_metrics_api.non_article_page_views_provider import (
+    NonArticlePageViewsProvider
+)
+
+
+CONTENT_TYPE_1: ContentTypeLiteral = 'blog-article'
+CONTENT_ID_1 = 'content_id_1'
+
+
+@pytest.fixture(name='redis_client_mock', autouse=True)
+def _redis_client_mock() -> MagicMock:
+    return MagicMock(name='redis_client')
+
+
+@pytest.fixture(name='redis_client_get_mock')
+def _redis_client_set_mock(redis_client_mock: MagicMock) -> MagicMock:
+    return redis_client_mock.get
+
+
+@pytest.fixture(name='non_article_page_views_provider')
+def _non_article_page_views_provider(
+    redis_client_mock: MagicMock
+) -> NonArticlePageViewsProvider:
+    return NonArticlePageViewsProvider()
+
+
+class TestNonArticlePageViewsProvider:
+    def test_should_always_return_empty_periods(
+        self,
+        non_article_page_views_provider: NonArticlePageViewsProvider,
+        redis_client_get_mock: MagicMock
+    ):
+        redis_client_get_mock.return_value = None
+        response = non_article_page_views_provider.get_page_views_by_content_type(
+            content_type='blog-article',
+            content_id=CONTENT_ID_1
+        )
+        assert response == {
+            'totalPeriods': 0,
+            'totalValue': ANY,
+            'periods': []
+        }
+
+    def test_should_return_zero_total_value_if_not_in_redis(
+        self,
+        non_article_page_views_provider: NonArticlePageViewsProvider,
+        redis_client_get_mock: MagicMock
+    ):
+        redis_client_get_mock.return_value = None
+        response = non_article_page_views_provider.get_page_views_by_content_type(
+            content_type='blog-article',
+            content_id=CONTENT_ID_1
+        )
+        assert response['totalValue'] == 0
+
+    def test_should_return_non_article_page_views_value_from_redis(
+        self,
+        non_article_page_views_provider: NonArticlePageViewsProvider,
+        redis_client_get_mock: MagicMock
+    ):
+        redis_client_get_mock.return_value = '123'
+        response = non_article_page_views_provider.get_page_views_by_content_type(
+            content_type=CONTENT_TYPE_1,
+            content_id=CONTENT_ID_1
+        )
+        assert response['totalValue'] == 123
+        redis_client_get_mock.assert_called_once_with(
+            f'non-article:{CONTENT_TYPE_1}:{CONTENT_ID_1}:page_views'
+        )
