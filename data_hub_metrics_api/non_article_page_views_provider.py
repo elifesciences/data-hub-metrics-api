389ec0121cb8e1018a6ee3c81d0cd0db4367ba3e
HazalCiplak <hazalciplak@gmail.com>
2025-07-14 16:25:25 +0100

    Implemented puting data into redis for non-articles

--- data_hub_metrics_api/non_article_page_views_provider.py
+++ data_hub_metrics_api/non_article_page_views_provider.py
@@ -1,11 +1,16 @@
 
 import logging
+from pathlib import Path
 from typing import Literal, Optional
+
+from tqdm import tqdm
 
 from data_hub_metrics_api.api_router_typing import (
     ContentTypeLiteral,
     MetricTimePeriodResponseTypedDict
 )
+from data_hub_metrics_api.sql import get_sql_path
+from data_hub_metrics_api.utils.bigquery import get_bq_result_from_bq_query
 
 LOGGER = logging.getLogger(__name__)
 
@@ -13,9 +18,14 @@ LOGGER = logging.getLogger(__name__)
 class NonArticlePageViewsProvider:
     def __init__(
         self,
-        redis_client
+        redis_client,
+        gcp_project_name: str = 'elife-data-pipeline'
     ):
         self.redis_client = redis_client
+        self.gcp_project_name = gcp_project_name
+        self.non_article_page_view_totals_query = Path(
+            get_sql_path('non_article_page_view_totals_query.sql')
+        ).read_text(encoding='utf-8')
 
     def get_page_views_by_content_type(
         self,
@@ -37,3 +47,19 @@ class NonArticlePageViewsProvider:
             'totalValue': int(redis_value or 0),
             'periods': []
         }
+
+    def refresh_non_article_page_view_totals(self) -> None:
+        LOGGER.info('Refreshing non-article page view totals data from BigQuery...')
+        bq_result = get_bq_result_from_bq_query(
+            project_name=self.gcp_project_name,
+            query=self.non_article_page_view_totals_query
+        )
+        total_rows = bq_result.total_rows
+        LOGGER.info('Total rows from BigQuery: %d', total_rows)
+
+        for row in tqdm(bq_result, total=total_rows, desc="Loading Redis"):
+            self.redis_client.set(
+                f'non-article:{row["content_type"]}:{row["content_id"]}:page_views',
+                row['page_view_count']
+            )
+        LOGGER.info('Done: Refreshing non-article page view totals data from BigQuery')
--- tests/unit_test/non_article_page_views_provider_test.py
+++ tests/unit_test/non_article_page_views_provider_test.py
@@ -1,7 +1,9 @@
-from unittest.mock import ANY, MagicMock
+from typing import Iterator
+from unittest.mock import ANY, MagicMock, patch
 
 import pytest
 
+import data_hub_metrics_api.non_article_page_views_provider as provider_module
 from data_hub_metrics_api.api_router_typing import ContentTypeLiteral
 from data_hub_metrics_api.non_article_page_views_provider import (
     NonArticlePageViewsProvider
@@ -17,8 +19,13 @@ def _redis_client_mock() -> MagicMock:
     return MagicMock(name='redis_client')
 
 
-@pytest.fixture(name='redis_client_get_mock')
+@pytest.fixture(name='redis_client_set_mock')
 def _redis_client_set_mock(redis_client_mock: MagicMock) -> MagicMock:
+    return redis_client_mock.set
+
+
+@pytest.fixture(name='redis_client_get_mock')
+def _redis_client_get_mock(redis_client_mock: MagicMock) -> MagicMock:
     return redis_client_mock.get
 
 
@@ -27,6 +34,15 @@ def _non_article_page_views_provider(
     redis_client_mock: MagicMock
 ) -> NonArticlePageViewsProvider:
     return NonArticlePageViewsProvider(redis_client_mock)
+
+
+@pytest.fixture(name='get_bq_result_from_bq_query_mock', autouse=True)
+def _get_bq_result_from_bq_query_mock() -> Iterator[MagicMock]:
+    with patch.object(
+        provider_module,
+        'get_bq_result_from_bq_query'
+    ) as mock:
+        yield mock
 
 
 class TestNonArticlePageViewsProvider:
@@ -71,4 +87,28 @@ class TestNonArticlePageViewsProvider:
         assert response['totalValue'] == 123
         redis_client_get_mock.assert_called_once_with(
             f'non-article:{CONTENT_TYPE_1}:{CONTENT_ID_1}:page_views'
+        )
+
+    def test_should_put_page_view_total_in_redis(
+        self,
+        get_bq_result_from_bq_query_mock: MagicMock,
+        non_article_page_views_provider: NonArticlePageViewsProvider,
+        redis_client_set_mock: MagicMock
+    ):
+        mock_bq_result = MagicMock()
+        mock_bq_result.total_rows = 1
+        mock_bq_result.__iter__.return_value = iter([{
+            'content_type': CONTENT_TYPE_1,
+            'content_id': CONTENT_ID_1,
+            'page_view_count': 123
+        }])
+        get_bq_result_from_bq_query_mock.return_value = mock_bq_result
+        non_article_page_views_provider.refresh_non_article_page_view_totals()
+        get_bq_result_from_bq_query_mock.assert_called_with(
+            project_name=non_article_page_views_provider.gcp_project_name,
+            query=non_article_page_views_provider.non_article_page_view_totals_query
+        )
+        redis_client_set_mock.assert_called_once_with(
+            f'non-article:{CONTENT_TYPE_1}:{CONTENT_ID_1}:page_views',
+            123
         )
