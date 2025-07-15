940a5e37891f16ac023d84f96bf7b2b30bb4d1b3
Hazal <HazalCiplak@users.noreply.github.com>
2025-07-15 17:30:39 +0100

    Add other non-article page view to the API (#40)
    * Enhance non-article page view totals query to include additional content types
    
    * Made content id more strict to avoid matching paths that are added somewhere at the end
    
    ---------
    
    Co-authored-by: Daniel Ecer <de-code@users.noreply.github.com>

--- data_hub_metrics_api/sql/non_article_page_view_totals_query.sql
+++ data_hub_metrics_api/sql/non_article_page_view_totals_query.sql
@@ -9,9 +9,18 @@ FROM (
     SUM(unique_session_count) AS page_view_count
   FROM (
     SELECT
-      REGEXP_EXTRACT(page_location, r'/(?:inside-elife)/([a-z0-9]+)/') AS content_id,
+      REGEXP_EXTRACT(
+        REGEXP_EXTRACT(page_location, r'://(?:[^/]+)(/[^?]*)'),  -- page path
+        r'^/(?:inside-elife|labs|collections|digests|events|interviews|for-the-press)/([a-z0-9]+)/'
+      ) AS content_id,
       CASE
         WHEN top_level_page_path = '/inside-elife' THEN 'blog-article'
+        WHEN top_level_page_path = '/labs' THEN 'labs-post'
+        WHEN top_level_page_path = '/collections' THEN 'collection'
+        WHEN top_level_page_path = '/digests' THEN 'digest'
+        WHEN top_level_page_path = '/events' THEN 'event'
+        WHEN top_level_page_path = '/interviews' THEN 'interview'
+        WHEN top_level_page_path = '/for-the-press' THEN 'press-package'
       END AS content_type,
       unique_session_count
     FROM `elife-data-pipeline.prod.ga4_metrics_event_counts_by_date`
