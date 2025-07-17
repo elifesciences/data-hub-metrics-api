6635fb6e9a99169e12c9f3ca5217004ea5902346
HazalCiplak <hazalciplak@gmail.com>
2025-07-17 12:35:29 +0100

    Move redis_client_mock fixture to conftest.py

--- tests/unit_test/conftest.py
+++ tests/unit_test/conftest.py
@@ -25,3 +25,8 @@ def mock_env() -> Iterator[dict]:
 def _bigquery_mock() -> Iterator[MagicMock]:
     with patch.object(bigquery_module, "bigquery") as mock:
         yield mock
+
+
+@pytest.fixture(name='redis_client_mock', autouse=True)
+def _redis_client_mock() -> MagicMock:
+    return MagicMock(name='redis_client')
