615c1121a3d1660aa002dc896f091939233bc6df
HazalCiplak <hazalciplak@gmail.com>
2025-07-17 12:15:31 +0100

    Refactor iter_with_progress to use a generic type for improved flexibility

--- data_hub_metrics_api/utils/progress_bar.py
+++ data_hub_metrics_api/utils/progress_bar.py
@@ -1,6 +1,8 @@
-from typing import Iterable
+from typing import Iterable, TypeVar
 from tqdm import tqdm
 
+T = TypeVar('T')
 
-def iter_with_progress(data: Iterable[dict], total: int, desc: str) -> Iterable[dict]:
+
+def iter_with_progress(data: Iterable[T], total: int, desc: str) -> Iterable[dict]:
     return tqdm(data, total=total, desc=desc)
